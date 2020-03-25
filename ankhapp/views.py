85f3e4e1afd6484526bdf6390e52ce216b22905e
sudhakae2797 <sudhakar.it.12345@gmail.com>
2020-03-25 18:31:47 +0530

    welcome api created

--- ankhapp/views.py
+++ ankhapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class WelcomeAPIView(APIView):
+    """Test API Views"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'use HTTTP methods as function(get,post,patch,put,delete)'
+            'is similar to django traditional view'
+            'give u most control over application logic'
+            'is mapped manually to urls'
+        ]
+        return Response({'message': 'Hello!','an_apiview': an_apiview})
