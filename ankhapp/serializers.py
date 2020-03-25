c2c1cfca3b10a5d4b4d1fdcae45b01599150bbe2
sudhakae2797 <sudhakar.it.12345@gmail.com>
2020-03-25 18:46:05 +0530

    Added post call

---
+++ ankhapp/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+
+class WelcomeSerializer(serializers.Serializer):
+    """serializer a name field for testing our APIView"""
+    name=serializers.CharField(max_length=10)
--- ankhapp/views.py
+++ ankhapp/views.py
@@ -1,10 +1,14 @@
 # Create your views here.
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from ankhapp.serializers import WelcomeSerializer
 
 
 class WelcomeAPIView(APIView):
     """Test API Views"""
+    serializer_class=WelcomeSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -15,3 +19,14 @@ class WelcomeAPIView(APIView):
             'is mapped manually to urls'
         ]
         return Response({'message': 'Hello!','an_apiview': an_apiview})
+
+    def post(self,request):
+        """Returns data with post call"""
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Welcome {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
